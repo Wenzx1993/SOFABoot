e7f7274b5b5cc16b5d644cb201d46a44b4b2b67f
文志勋 <1006380802@qq.com>
2023-02-23 17:26:00 +0800

    1.初始化sofaboot项目 2.新增发布、消费、接口、运行模块

---
+++ SofaBootDemo1/SofaBootFacada/src/test/java/com/example/sofabootfacada/SofaBootFacadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sofabootfacada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofaBootFacadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ SofaBootDemo1/SofaBootRun/src/main/java/com/example/sofabootrun/test/Test.java
@@ -0,0 +1,12 @@
+package com.example.sofabootrun.test;
+
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
+public class Test {
+
+    public static void main(String[] args) throws IOException {
+    }
+}
